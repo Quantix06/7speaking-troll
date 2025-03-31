899bca64cc0097fc7a92bf4bd415ce26b494719b
Quantix06 <marchalquentin06@gmail.com>
2025-03-31 11:54:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginForm from "./Pages/LoginForm";
 import Password from "./Components/Password";
-
+import Resolve from "./Components/Resolve";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<LoginForm />}></Route>
         <Route path="/cesar" element={<LoginForm />}></Route>
         <Route path="/password" element={<Password />} />
+        <Route path="/resolve" element={<Resolve />} />
       </Routes>
     </BrowserRouter>
   );
