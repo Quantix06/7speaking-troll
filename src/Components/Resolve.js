e7d59d7dbc3f2c4a53b81e890b1fa39da75434b3
Cyllia <germaincyllia@gmail.com>
2025-03-31 14:24:13 +0200

    Resolve

--- src/Components/Resolve.js
+++ src/Components/Resolve.js
@@ -5,11 +5,9 @@ const Resolve = () => {
   return (
     <div className="quiz-interface-container">
       <header className="quiz-header">
-        <div className="logo">
-          <img src="https://lms.7speaking.com/nasimages/1800words/logo.png" alt="logo" className="logo-image" />
-          <h6 className="quiz-title">Les nombres</h6>
-        </div>
-      </header> 
+        <img src="7speaking_logo.png" alt="logo" className="logo-svg"/>
+        <h6 className="quiz-title">Les nombres</h6>
+      </header>
       <div className="quiz-container">
         <div className="question-card">
           <h2 className="question-title">2</h2>
@@ -19,6 +17,7 @@ const Resolve = () => {
             <button className="answer-button">twelve</button>
             <button className="answer-button">fourteen</button>
           </div>
+          <button className="primary-btn">Valider</button>
         </div>
       </div>
     </div>
