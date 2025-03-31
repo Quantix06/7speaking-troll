a86345c0b10684b853718f3c3172afe57e158392
Quantix06 <marchalquentin06@gmail.com>
2025-03-31 10:06:36 +0200

    Add Resolve component and update routing in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage";
 import Password from "./Components/Password";
-
+import Resolve from "./Components/Resolve";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/Cesar" element={<HomePage/>}></Route>
         <Route path="/Password" element={<Password />} /> 
+        <Route path="/Resolve" element={<Resolve/>} /> 
 
       </Routes>
     </BrowserRouter>
